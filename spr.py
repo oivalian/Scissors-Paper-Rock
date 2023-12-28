a1c54addcfd2dacfccfecca99c35ddcfbccd7c60
oivalian <109859213+oivalian@users.noreply.github.com>
2023-12-29 02:02:09 +1100

    Simplified spr.py

--- spr.py
+++ spr.py
@@ -1,65 +1,29 @@
-import random
+import random as r
 import time as t
 import os
 
+
 def game(score, cpu_score):
     while True:
-        print(f'\nYour score is: {score}\nThe computers score is: {cpu_score}\n')
-        print('\n[P] Paper\n[S] Scissors\n[R] Rock\n')
-        player_move = input('Make a selection > ')
-        match player_move:
-            case 'p' | 'P':
-                cpu_move = random.randrange(1, 4)
-                if cpu_move == 1:
-                    print("Computer used paper. It's a tie game!")
-                    t.sleep(1.3)
-                    os.system('cls')
-                elif cpu_move == 2:
-                    print("Computer used scissors. You lose! ")
-                    t.sleep(1.3)
-                    os.system('cls')
-                    cpu_score += 1
-                elif cpu_move == 3:
-                    print("Computer used rock. You win!")
-                    t.sleep(1.3)
-                    os.system('cls')
-                    score += 1
-            case 's' | 'S':
-                cpu_move = random.randrange(1, 4)
-                if cpu_move == 1:
-                    print("Computer used paper. You win!")
-                    t.sleep(1.3)
-                    os.system('cls')
-                    score += 1
-                elif cpu_move == 2:
-                    print("Computer used scissors. It's a tie game! ")
-                    t.sleep(1.3)
-                    os.system('cls')
-                elif cpu_move == 3:
-                    print("Computer used rock. You lose!")
-                    t.sleep(1.3)
-                    os.system('cls')
-                    cpu_score += 1
-            case 'r' | 'R':
-                cpu_move = random.randrange(1, 4)
-                if cpu_move == 1:
-                    print("Computer used paper. You lose!")
-                    t.sleep(1.3)
-                    os.system('cls')
-                    cpu_score += 1
-                elif cpu_move == 2:
-                    print("Computer used scissors. You win! ")
-                    t.sleep(1.3)
-                    os.system('cls')
-                    score += 1
-                elif cpu_move == 3:
-                    print("Computer used rock. It's a tie game!")
-                    t.sleep(1.3)
-                    os.system('cls')
-            case _:
-                os.system('cls')
+        print(f"\nYour score is: {score}\nThe computers score is: {cpu_score}")
+        print("\nPaper\nScissors\nRock\n")
+        player_move = input("Make a selection > ")
+        player_move = player_move.lower()
+        cpu_move = r.choice(choices)
+        if cpu_move == player_move:
+            print(f"It's a tie! You both entered in {player_move}!")
+        elif (cpu_move == "rock" and player_move == "paper") or \
+                (cpu_move == "scissors" and player_move == "rock") or \
+                (cpu_move == "paper" and player_move == "scissors"):
+            score += 1
+            print(f"You win! {player_move} beats {cpu_move}!")
+        else:
+            cpu_score += 1
+            print(f"You lose! Computer beat you with {cpu_move}!")
+        t.sleep(1.3)
+        os.system('cls')
 
-
+choices = ["rock", "paper", "scissors"]
 score = 0
 cpu_score = 0
 game(score, cpu_score)

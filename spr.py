b14935cba24965668d2cc47900341ccad6e73519
oivalian <109859213+oivalian@users.noreply.github.com>
2024-10-15 14:02:45 +1100

    Minor tweaks

--- spr.py
+++ spr.py
@@ -1,29 +1,27 @@
-import random as r
-import time as t
-import os
+from os import system
+from random import choice
+from time import sleep
 
-
-def game(score, cpu_score):
+def game(player, cpu):
     while True:
-        print(f"\nYour score is: {score}\nThe computers score is: {cpu_score}")
+        print(f"\nYour score is: {player}\nThe computers score is: {cpu}")
         print("\nPaper\nScissors\nRock\n")
         player_move = input("Make a selection > ")
         player_move = player_move.lower()
-        cpu_move = r.choice(choices)
+        cpu_move = choice(choices)
         if cpu_move == player_move:
             print(f"It's a tie! You both entered in {player_move}!")
         elif (cpu_move == "rock" and player_move == "paper") or \
                 (cpu_move == "scissors" and player_move == "rock") or \
                 (cpu_move == "paper" and player_move == "scissors"):
-            score += 1
+            player += 1
             print(f"You win! {player_move} beats {cpu_move}!")
         else:
-            cpu_score += 1
+            cpu += 1
             print(f"You lose! Computer beat you with {cpu_move}!")
-        t.sleep(1.3)
-        os.system('cls')
+        sleep(1.3)
+        system('cls')
 
 choices = ["rock", "paper", "scissors"]
-score = 0
-cpu_score = 0
+score, cpu_score = 0, 0
 game(score, cpu_score)
